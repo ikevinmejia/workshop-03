9faf2fa41f573ff6941ec12d150c60d069f65fee
Kevin Mejía <kevinarley6958@gmail.com>
2022-08-16 17:03:34 -0500

    context provider main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,15 @@ import App from "./App";
 import { store, persistor } from "./Redux/store/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
+import Context from "./context/Context";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <Context>
+          <App />
+        </Context>
       </PersistGate>
     </Provider>
   </React.StrictMode>,
